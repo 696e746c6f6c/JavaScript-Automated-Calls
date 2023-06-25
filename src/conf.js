7747cff643678f0b9b247084e5556ccaad450368
int <96709073+696e746c6f6c@users.noreply.github.com>
2023-06-25 23:31:18 +0200

    Update conf.js

--- src/conf.js
+++ src/conf.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
 
+const x = () => {
+    const subdomains = fs.readFileSync('subdomains.txt', 'utf8').split('\n');
+
+    return {
+        // This is a Proxy Config that we will need
+        // proxy: {
+        //     host: "127.0.0.1",
+        //     port: 8888
+        // },
+
+        payloadFile: "xsstesting/payload.txt",
+        fileOutput: false,
+        subdomains,
+        port: [80, 443], // Here we specify multiple ports as an array, 8080 port is same as 80 port but however if there's something going on feel free to modify conf.js file.
+        path: "/param.plp?page={0}",
+        method: ["POST", "GET"], // Here we specify multiple methods as an array
+        protocol: "https:",
+        postData: "paramName1={0}&paramName2=paramValue2",
+    };
+};
+
+module.exports = {
+    x,
+};

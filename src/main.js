f850b7ab4daa6e746ffb3cc8aece73770072f83c
int <96709073+696e746c6f6c@users.noreply.github.com>
2023-06-25 23:17:21 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-const http = require('http');
-const { ArgumentParser } = require('argparse');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const jsesc = require('jsesc');
-const fs = require('fs');
-const config = require('./config');
+const { ArgumentParser } = require('argparse');
+
+// Here we create an argument parser using ArgumentParser() constructor function that is called on an parser object
+const parser = new ArgumentParser({
+    description: 'JavaScript Calls Automation'
+});
+
+parser.add_argument('-v', '--version', { action: 'version', version });
+parser.add_argument('-t', '--target', { help: 'URL of the app', required: true });
+
+// This is simple, here parse_args() method is called on the parser object to parse the command-line arguments
+const args = parser.parse_args();
